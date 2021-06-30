8a144023bb9f93faf9ff07c0fbb8800d1ed5a281
Héctor Banderas <31503660+Hectorban@users.noreply.github.com>
2021-06-30 18:13:50 -0500

    Made some quick backend that registers the observer keys

--- src/extension/keypress.ts
+++ src/extension/keypress.ts
@@ -1,12 +1,27 @@
-import ioHook from 'iohook';
+import ioHook from 'iohook'
 import * as nodecgApiContext from './utils/nodecg-api-context'
+import keyReference from './utils/keyReference.json'
 
 const nodecg = nodecgApiContext.get()
 const keypresRep = nodecg.Replicant("keypressRep")
-console.log("a")
-ioHook.on('mousemove', (event) => {
-  console.log(event); // { type: 'mousemove', x: 700, y: 400 }
+const onOffRep = nodecg.Replicant("onOffRep")
+
+ioHook.on('keydown', (event) => {
+  const {rawcode} = event
+  const keyPressString = String.fromCharCode(rawcode)
+  console.log(rawcode)
+  if(rawcode >= 48 && rawcode <= 57) {
+    keypresRep.value = keyPressString
+    onOffRep.value = "on"
+  } 
+  if(rawcode >= 96 && rawcode <= 105) {
+    keypresRep.value = keyReference[keyPressString]
+    onOffRep.value = "on"
+  } 
+  if(rawcode === 67) {
+    onOffRep.value = "off"    
+  }
 });
 
 // Register and start hook
-ioHook.start();
+ioHook.start()
